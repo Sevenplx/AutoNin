d801622b6ad18e54df0512ddaddf64e7f9075b76
Sevenplx <techwin383@gmail.com>
2025-07-09 12:50:14 +0530

    gunicorn.conf.py update

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,5 @@
 timeout = 120
 workers = 1
 threads = 1
-# This increases the timeout to 2 minutes and avoids spamming the server with too many workers on limited RAM.
+# This increases the timeout to 2 minutes and avoids spamming the server
+# with too many workers on limited RAM.
